3259c19238bf7633e6c60d479b0f79ceb91031e9
Priyanshu Rajpurohit <officialpriyanshu14@gmail.com>
2025-10-24 11:59:46 +0530

    Add Project.js model file

---
+++ server/models/Project.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String
+  },
+  technologies: [{
+    type: String
+  }],
+  githubUrl: {
+    type: String
+  },
+  liveUrl: {
+    type: String
+  },
+  featured: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Project', projectSchema);
